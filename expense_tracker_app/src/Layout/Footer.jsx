34efd13f01bd68b41374c37b4603a79b054dc7a1
popcari <duy.nt1009@gmail.com>
2024-09-28 10:30:53 +0700

    [ fix ] - footer navigation UI

--- expense_tracker_app/src/Layout/Footer.jsx
+++ expense_tracker_app/src/Layout/Footer.jsx
@@ -97,7 +97,6 @@ export default function Footer() {
 							height='32px'
 							viewBox='0 0 24.00 24.00'
 							xmlns='http://www.w3.org/2000/svg'
-							stroke='#c4c4c4'
 						>
 							<g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
 							<g
